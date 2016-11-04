c2b4da6e7e53bf83f0faed592911e31efe9adfa8
omjoko <heru.m175@gmail.com>
2016-11-05 04:18:02 +0700

    this apps
    adasdasdzxcz

---
+++ resources/views/layouts/slice/sidebar.blade.php
@@ -0,0 +1,50 @@
+      <!--sidebar start-->
+      <aside>
+          <div id="sidebar"  class="nav-collapse ">
+              <!-- sidebar menu start-->
+              <ul class="sidebar-menu" id="nav-accordion">
+                  <li class="sub-menu">
+                      <a href="javascript:;">
+                          <i class="fa fa-list"></i>
+                          <span>Master Data</span>
+                      </a>
+                       <ul class="sub">
+                          <li><a>Ingredient</a></li>
+                          <li><a>Recipe</a></li>
+                          <li><a>Menu</a></li>
+                          <li><a>User</a></li>
+                          <li><a>Vendor</a></li>
+                          <li><a>Cruise</a></li>
+                          <li><a>Voyage Class</a></li>
+                          <li><a>Route</a></li>
+                      </ul>
+                  </li>
+                  <li>
+                      <a href="/">
+                          <i class="fa fa-paper-plane"></i>
+                          <span>Voyage Planning</span>
+                      </a>
+                  </li>                  
+                  <li>
+                      <a href="/">
+                          <i class="fa fa-cube"></i>
+                          <span>Draft PO / Requisition</span>
+                      </a>
+                  </li>                  
+                  <li>
+                      <a href="/">
+                          <i class="fa fa-sign-out"></i>
+                          <span>Inventory Out</span>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="/">
+                          <i class="fa fa-spoon"></i>
+                          <span>Waste</span>
+                      </a>
+                  </li>
+              </ul>
+              <!-- sidebar menu end-->
+          </div>
+      </aside>
+      <!--sidebar end-->
