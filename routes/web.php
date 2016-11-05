8063441c45a0f9ea40561d0c1d8acc8739846023
broadwell-23 <farfarizky@live.com>
2016-11-05 23:11:50 +0700

    Masih di User Management

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,5 @@ Route::get('/ingredients', 'ingredientsController@sedotData');
 Route::get('/logout', 'HomeController@logout');
 
 // Broadwell-23 code...
-	// User Management
-	Route::resource('/user', 'UserController');
+// User Management
+Route::resource('/user', 'UserController');
