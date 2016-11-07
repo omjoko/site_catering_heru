0cb2a888e741b908c0c3e906218f5d01da412ee5
broadwell-23 <farfarizky@live.com>
2016-11-07 16:52:03 +0700

    new Table
    Rutes and Transits

--- app/Http/Controllers/RuteController.php
+++ app/Http/Controllers/RuteController.php
@@ -6,5 +6,80 @@ use Illuminate\Http\Request;
 
 class RuteController extends Controller
 {
-    //
+    public function index()	{
+
+    	$rutes = DB::table('rutes')
+                    ->get();
+
+    	return view('user', ['users' => $users, 'counts' => $counts]);
+    }
+
+    public function store(Request $request) {
+
+    	$data = $request->all();
+
+        $requestDate = $data['tgl_valid'];
+        $newDate = date("Y-m-d", strtotime($requestDate));
+
+    	DB::table('users')->insert([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'email1' => $data['email1'],
+            'password' => bcrypt($data['password']),
+            'telepon' => $data['telepon'],
+            'no_nrp' => $data['no_nrp'],
+            'no_bk' => $data['no_bk'],
+            'no_sijil' => $data['no_sijil'],
+            'sertifikat' => $data['sertifikat'],
+            'tgl_valid' => $newDate,
+            'privilege' => $data['privilege'],
+        ]);    
+
+    	return redirect()->action('UserController@index');
+    }
+
+    public function update(Request $request) {
+
+    	$data = $request->all();
+
+    	if($request->has('password')) {
+            $pass = bcrypt($data['password']);
+        } else {
+            $oldpass = DB::table('users')
+                        ->where('id', $data['id'])
+                        ->get();
+            foreach ($oldpass as $oldpass) {
+                $pass = $oldpass->password;
+            }
+        }
+
+        DB::table('users')
+                ->where('id', $data['id'])
+                ->update([
+                    'name' => $data['name'],
+		            'email' => $data['email'],
+		            'email1' => $data['email1'],
+                    'password' => $pass,
+		            'telepon' => $data['telepon'],
+		            'no_nrp' => $data['no_nrp'],
+		            'no_bk' => $data['no_bk'],
+		            'no_sijil' => $data['no_sijil'],
+		            'sertifikat' => $data['sertifikat'],
+		            'tgl_valid' => $data['tgl_valid'],
+		            'privilege' => $data['privilege'],
+                    ]);
+
+    	return redirect()->action('UserController@index');
+    }
+
+    public function destroy(Request $request) {
+
+    	$id = $request->id;
+
+        DB::table('users')
+                ->where('id', $id)
+                ->delete();
+
+    	return redirect()->action('UserController@index');
+    }
 }
