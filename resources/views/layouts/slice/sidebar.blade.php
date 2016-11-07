0f0bca6d320705caf47451f3a05a0b18e15855bf
broadwell-23 <farfarizky@live.com>
2016-11-08 03:59:12 +0700

    clean sidebar

--- resources/views/layouts/slice/sidebar.blade.php
+++ resources/views/layouts/slice/sidebar.blade.php
@@ -4,32 +4,32 @@
               <!-- sidebar menu start-->
               <ul class="sidebar-menu" id="nav-accordion">
                   <li class="sub-menu">
-                      <a href="javascript:;" class="{{ Request::is('user', 'recipes', 'ingredients') ? 'active' : '' }}">
+                      <a href="javascript:;" class="{{ Request::is('user', 'recipes', 'ingredients', 'categorys', 'measurements', 'menus', 'vendors', 'kapal', 'penyimpanan', 'pelabuhan', 'rute') ? 'active' : '' }}">
                           <i class="fa fa-list"></i>
                           <span>Master Data</span>
                       </a>
                        <ul class="sub">
                           <li class="{{ Request::is('user') ? 'active' : '' }}"><a href="user">User</a></li>
                           <li>
-                              <a href="javascript:;">Ingredient</a>
+                              <a href="javascript:;" class="{{ Request::is('ingredients', 'categorys', 'measurements') ? 'active' : '' }}">Ingredient</a>
                               <ul class="sub">
-                                  <li><a  href="ingredients">Ingredient Table</a></li>
-                                  <li><a  href="categorys">Kategori Ingredient</a></li>
-                                  <li><a  href="measurements">Satuan Ingredient</a></li>
+                                  <li class="{{ Request::is('ingredients') ? 'active' : '' }}"><a  href="ingredients">Ingredient Table</a></li>
+                                  <li class="{{ Request::is('categorys') ? 'active' : '' }}"><a  href="categorys">Kategori Ingredient</a></li>
+                                  <li class="{{ Request::is('measurements') ? 'active' : '' }}"><a  href="measurements">Satuan Ingredient</a></li>
                               </ul>
                           </li>
                           <li class="{{ Request::is('recipes') ? 'active' : '' }}"><a href="recipes">Recipe</a></li>
-                          <li><a href="menus">Menu</a></li>
-                          <li><a href="vendors">Vendor</a></li>
+                          <li class="{{ Request::is('menus') ? 'active' : '' }}"><a href="menus">Menu</a></li>
+                          <li class="{{ Request::is('vendors') ? 'active' : '' }}"><a href="vendors">Vendor</a></li>
                           <li>
-                              <a href="javascript:;">Kapal</a>
+                              <a href="javascript:;" class="{{ Request::is('kapal', 'penyimpanan') ? 'active' : '' }}">Kapal</a>
                               <ul class="sub">
-                                  <li><a  href="kapal">Kapal</a></li>
-                                  <li><a  href="penyimpanan">Penyimpanan</a></li>
+                                  <li class="{{ Request::is('kapal') ? 'active' : '' }}"><a  href="kapal">Kapal</a></li>
+                                  <li class="{{ Request::is('penyimpanan') ? 'active' : '' }}"><a  href="penyimpanan">Penyimpanan</a></li>
                               </ul>
                           </li>
-                          <li><a href="pelabuhan">Pelabuhan</a></li>
-                          <li><a href="rute">Rute</a></li>
+                          <li class="{{ Request::is('pelabuhan') ? 'active' : '' }}"><a href="pelabuhan">Pelabuhan</a></li>
+                          <li class="{{ Request::is('rute') ? 'active' : '' }}"><a href="rute">Rute</a></li>
                       </ul>
                   </li>
                   <li>
@@ -38,8 +38,8 @@
                           <span>Voyage Planning</span>
                       </a>
                   </li> 
-                  <li>
-                      <a href="/food-plannings">
+                  <li class="{{ Request::is('food-plannings') ? 'active' : '' }}">
+                      <a href="food-plannings">
                           <i class="fa fa-spoon"></i>
                           <span>Food Planning</span>
                       </a>
@@ -50,8 +50,8 @@
                           <span>Draft PO / Requisition</span>
                       </a>
                   </li>
-                  <li>
-                      <a href="/invoices">
+                  <li class="{{ Request::is('invoices') ? 'active' : '' }}">
+                      <a href="invoices">
                           <i class="fa fa-file-text"></i>
                           <span>Invoice</span>
                       </a>
